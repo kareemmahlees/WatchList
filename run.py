81fa6a98fc16474d0a9b9ac7f716e27fc6cc7b3b
Kareem <kareemmahlees@gmail.com>
2022-04-18 23:43:35 +0200

    made the server run with the monitor

--- run.py
+++ run.py
@@ -9,12 +9,14 @@ movies and tv shows
 import os
 import _Constants
 from NotionApi import NotionClient
+from keep_alive import keep_alive
 
 # -------------------------------------
 os.system("cls" if os.name == "nt" else "clear")
 print("Monitoring..")
 try:
     client = NotionClient(_Constants.NOTION_TOKEN, _Constants.DATABASE_ID)
+    keep_alive()
     client.monitor_the_database()
 except KeyboardInterrupt:
     print("Closed Program !")
